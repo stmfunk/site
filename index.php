877c85f18cd2205daba2e5866abc16f37812b663
Donal O'Shea <renegadethunder@gmail.com>
2014-02-23 16:36:27 +0000

    Added google font

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
   <head>
     <title>Menishi</title>
     <link rel="icon" href="favicon.png" type="image/x-icon" />
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css' />
     <link rel="stylesheet" type="text/css" href="css/main.css" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   </head>

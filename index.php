57be2296a2cf86853512ff61dc216cf3de67b55c
Donal O'Shea <renegadethunder@gmail.com>
2014-02-23 15:59:41 +0000

    Fixed favicon

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Menishi</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  </head>
+  <body>
+

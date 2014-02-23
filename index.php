e1f3f5b8331c0e21b71a9c85dc07f26fcbe3bbe6
Donal O'Shea <renegadethunder@gmail.com>
2014-02-23 16:13:53 +0000

    Modified background color added git ignore

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html>
   <head>
     <title>Menishi</title>
-    <link rel="icon" href="favicon.png" type="image/x-icon">
+    <link rel="icon" href="favicon.png" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   </head>
   <body>
+  </body>
+</html>
 

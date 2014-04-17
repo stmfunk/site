f92f1946ff988d987a7d39db9f64401715a14ea2
Donal O'Shea <renegadethunder@gmail.com>
2014-04-17 15:23:14 +0100

    Started building a more basic blog

--- main-nav.php
+++ main-nav.php
@@ -1,18 +1,18 @@
     <nav class="group app-prefs">
       <div id="first-item">
-          <span id="first-item" class="sliding-select"><h1>Home</h1></span>
+         <span id="home" class="sliding-select"><h1>Home</h1></span>
       </div>
       <div>
-        <a href="#"><span class="sliding-select"><h1>Projects</h2></span></a>
+         <a href="#"><span id="projects-tab" class="sliding-select"><h1>Projects</h2></span></a>
       </div>
       <div>
-        <a href="#"><span class="sliding-select"><h1>Downloads</h2></span></a>
+         <a href="#"><span id="downloads-tab" class="sliding-select"><h1>Downloads</h2></span></a>
       </div>
       <div>
-        <a href="#"><span class="sliding-select"><h1>Wiki</h2></span></a>
+        <a href="#"><span id="wiki-tab" class="sliding-select"><h1>Wiki</h2></span></a>
       </div>
       <div>
-        <a href="#"><span class="sliding-select"><h1>Blog</h2></span></a>
+        <a href="#"><span id="blog-tab" class="sliding-select"><h1>Blog</h2></span></a>
       </div>
       </ul>
     </nav>
